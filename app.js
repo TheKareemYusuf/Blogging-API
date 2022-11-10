ceb698e45d6cd570acacf30b4addf3ec27535b4c
Yusuf <yakande96@gmail.com>
2022-11-10 17:41:06 +0100

    set home response

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const passport = require("passport");
-const bodyParser = require("body-parser");
+const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const blogRouter = require("./routes/blogRoutes");
 const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
 
-require("./authentication/auth");
+require('./authentication/auth')
 
 const app = express();
 
@@ -14,10 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+
+
+app.get('/', (req, res) => {
+    res.json({
+      status: 'status',
+      message: 'Visit the following link(s) for details about usage',
+      link: '/link to come here',
+      readme: '/readme link here',
+    })
+  })
 // ROUTES
 
 app.use("/api/v1/", authRouter);
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/users", userRouter);
+
+
 
 module.exports = app;
